7c7e8cc6d602d95bc8628c5c543bfc6ceb11df33
v <agentmi6@gmail.com>
2016-07-19 12:13:20 +0200

    gameDb

---
+++ App/ProgramBackUp - Copy.cs
@@ -0,0 +1,228 @@
+﻿using GameModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        public static List<Game> games = new List<Game>();
+
+        static void Main(string[] args)
+        {
+            StartApp();
+        }
+
+        private static void StartApp()
+        {
+            DisplayMenu();
+            int choice = int.Parse(Console.ReadLine());
+            bool menuFlag = false;
+
+            while (!menuFlag)
+            {                
+                if (choice == 1)
+                { 
+                    Console.Clear();
+                    AddGame();
+                    menuFlag = true;
+                }
+                else if (choice == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Game List: ");
+                    Console.WriteLine(" ");
+                    CheckList();
+                    menuFlag = true;
+                }
+                else if (choice == 3)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You can add more games later.");
+                    Console.WriteLine(" ");
+                    menuFlag = true;
+                }                                        
+            }
+        }
+
+        private static void DisplayMenu()
+        {
+            Console.WriteLine("MENU");
+            Console.WriteLine(" ");
+            Console.WriteLine("1. Add Game");
+            Console.WriteLine("2. Check Game List");
+            Console.WriteLine("3. Exit");
+        }
+
+        private static void AddGame()
+        {
+            //Console.Clear();
+            int gameIndex = 0;
+            addGameLabel:
+            Console.WriteLine("Adding game...");
+            Console.WriteLine(" ");
+            //List<Game> games = new List<Game>();
+            bool gameFlag = false;
+
+            while (!gameFlag)
+            {   
+                             
+                games.Add(new Game());
+                Console.Write("Add game name: ");
+                games[gameIndex].GameName = Console.ReadLine();
+
+                Console.Write("Add game platform: ");
+                games[gameIndex].Platform = Console.ReadLine();
+
+                Console.Write("Add year of release: ");
+                games[gameIndex].Year = Console.ReadLine();
+
+                Console.Write("Add short game summary: ");
+                games[gameIndex].GameSummary = Console.ReadLine();
+
+                bool priceFlag = false;
+                int number = 0;
+
+                while (!priceFlag)
+                {
+                    Console.Write("Add game price: ");
+                    if (int.TryParse(Console.ReadLine(), out number))
+                    {
+                        games[gameIndex].Price = number;
+                        priceFlag = true;
+                        gameFlag = true;
+                        Console.Clear();
+                        Console.WriteLine("Game added!");
+                        Console.WriteLine();                                               
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} was not a valid input for game price, please try again.",number);
+                    }
+                }                
+            }
+
+            Console.WriteLine("Enter \"B\" to go back and add new game or \"M\" to return to menu");
+            string goBack;
+            goBack = Console.ReadLine();
+
+            bool backFlag = false;
+
+            while (!backFlag)
+            {
+                if (goBack.ToLower() == "b")
+                {
+                    gameIndex++;
+                    backFlag = true;
+                    Console.Clear();
+                    goto addGameLabel;
+                }
+                else if (goBack.ToLower() == "m")
+                {                    
+                    backFlag = true;
+                    Console.Clear();
+                    StartApp();
+                }
+            }
+        }
+
+        private static bool CheckForAddedGames()
+        {
+            if (!games.Any())
+            {
+                Console.WriteLine("Currently there are no games in the list!");
+                Console.WriteLine();
+                Console.WriteLine("Press enter to go back");
+                Console.ReadLine();
+                Console.Clear();
+                StartApp();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private static void CheckList()
+        {           
+            if (!CheckForAddedGames())
+            {
+                return;
+            }
+            
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Game Name                     | Platform   | Year   |  Price");
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine();
+
+            foreach (Game game in games)
+            {
+                Console.WriteLine(string.Format("{0,-29} | {1,-10} | {2,-6} | {3,5}", game.GameName, game.Platform, game.Year, game.Price));
+            }            
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine();
+
+            bool backFlag = false;
+
+            while (!backFlag)
+            {
+                Console.WriteLine("Enter \"B\" to go back to the menu.");
+                Console.WriteLine("To sort the list by price press \"P\", or \"Y\" to sort by release year.");
+                string choice = Console.ReadLine();
+
+                if (choice.ToLower() == "b")
+                {
+                    Console.Clear();
+                    backFlag = true;
+                    StartApp();
+                }
+                else if (choice.ToLower() == "p")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Game List: ");
+                    Console.WriteLine();
+                    IEnumerable<Game> orderedGames =
+                        from game in games
+                        orderby game.Price descending
+                        select game;
+
+
+                    Console.WriteLine("------------------------------------------------------------");
+                    Console.WriteLine("Game Name                     | Platform   | Year   |  Price");
+                    Console.WriteLine("------------------------------------------------------------");
+                    Console.WriteLine();
+
+                    foreach (Game game in orderedGames)
+                    {
+                        Console.WriteLine(string.Format("{0,-29} | {1,-10} | {2,-6} | {3,5}", game.GameName, game.Platform, game.Year, game.Price));
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine("------------------------------------------------------------");
+                    Console.WriteLine();
+                }
+
+                //else if (choice.ToLower() == "y")
+                //{
+
+                //}
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("{0} is wrong input for return to menu", choice);
+                }
+            }
+
+            
+            
+
+
+        }
+    }
+}
---
+++ GameModel/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameModel
+{
+    public class Game
+    {       
+        public string GameName { get; set; }
+        public string Platform { get; set; }
+        public string Year { get; set; }
+        public double Price{ get; set; }        
+        public string GameSummary { get; set; }
+    }
+}
